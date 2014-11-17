5462d9ea1ef79d3fa6e01da88718dac725c88bdf
drew gordon <drew.s.gordon@gmail.com>
2014-11-17 15:38:54 -0500

    attempt to get a count of all names in the contect description field

--- tools/scripts/rutgers/ilm_nlp.py
+++ tools/scripts/rutgers/ilm_nlp.py
@@ -7,15 +7,35 @@ if sys.version_info < (3, 0):
     reload(sys)
     sys.setdefaultencoding("utf-8")
 import json
+from pprint import pprint
 
 input_file = sys.argv[1] #csv file
 
 '''try with hidden markov models pos tagger'''
 #_NEW_TAGGER = 'taggers/hmm_treebank_pos_tagger/treebank.tagger.pickle'
 
+def hasNum(s):
+
+    '''check if string has a number'''
+    return any(i.isdigit() for i in s)
+
+
 def makeSentenceDict(inputf):
     texts = {}
-    removables = {'\n': ' ', ':': '', '\t': ' ', '.': ''}
+    removables = {'\n': ' ',
+                  '\t': ' ', 
+                  ':': ' ',
+                  ';': ' ',     
+                  '.': '', 
+                  '/': ' ', 
+                  '(': ' ', 
+                  ')': ' ',
+                  '*': '', 
+                  '=': '',
+                  '-': ' ',
+                  '"': '',
+                  ',': ' ',
+                  "'s": ''}
 
     with open(inputf, 'rt') as csvfile:
         f = csv.reader(csvfile)
@@ -38,7 +58,9 @@ def makeSentenceDict(inputf):
 
 
 def tagText(textobj):
+
     names_in_texts = {}
+
 
     for k,v in textobj.items():
         key = k
@@ -52,12 +74,36 @@ def tagText(textobj):
 
         names_in_texts[key] = {'text': text, 'proper_nouns': proper_nouns}
 
+        
+    
+
     return names_in_texts
 
 
 
+def allUniqNNP(taggedobj):
 
 
-json_output = json.dumps(tagText(makeSentenceDict(input_file)), indent=4)
+    all_uniq_nnp = {}
 
-print(json_output)
+    for k,v in taggedobj.items():
+        proper_nouns = taggedobj[k]['proper_nouns']
+        for i in range(len(proper_nouns)):
+            if not hasNum(proper_nouns[i].lower()):
+                nnp = proper_nouns[i].lower()
+            if nnp in all_uniq_nnp:
+                all_uniq_nnp[nnp] += 1
+            else:
+                all_uniq_nnp[nnp] = 1
+
+                
+    return all_uniq_nnp
+
+tagged_texts = tagText(makeSentenceDict(input_file))
+
+json_output = json.dumps(tagged_texts, indent=4)
+
+all_nnp = allUniqNNP(tagged_texts)
+
+print(len(all_nnp))
+pprint(all_nnp)
