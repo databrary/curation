de6a82e6ee27efcd2ff41f2a0661f7bafd411389
drew gordon <drew.s.gordon@gmail.com>
2014-11-17 19:30:55 -0500

    position change

--- tools/scripts/csv2json.py
+++ tools/scripts/csv2json.py
@@ -121,7 +121,7 @@ def parseCSV2JSON(s_csvFile, p_csvFile):
 
             path = row[headerIndex['filepath']]
             clipArr = [row[headerIndex['clip_in']], row[headerIndex['clip_out']]] if row[headerIndex['clip_in']] != '' else ''
-            position = [row[headerIndex['position']]] if row[headerIndex['position']] != '' else ['auto']
+            position = [row[headerIndex['position']]] if row[headerIndex['position']] != '' else 'auto'
             classification = row[headerIndex['classification']].upper() if row[headerIndex['classification']] != '' else 'RESTRICTED'
             top = True if row[headerIndex['top']] != '' else False
             pilot = row[headerIndex['pilot']]
