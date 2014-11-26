c0e445a19e4ef9db5d9cfcb040b9ff2298eb5e6f
drew gordon <drew.s.gordon@gmail.com>
2014-11-26 11:18:47 -0500

    fix segment script so that it actually works
    though there is a problem with the list of pixels it should be picking up, way to many repeats

--- tools/scripts/gesell/image_segments.sh
+++ tools/scripts/gesell/image_segments.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-PIXEL_FILE=$1
-IMAGE_FILE=$2
-SHADOW_FILE=$3
+DIR=$1
+PIXEL_FILE="$DIR$2"
+IMAGE_FILE="$DIR$3"
+SHADOW_FILE="$DIR$4"
 START_POINT=()
 FIRST_PIXEL=false
 do_not_collect=false
 
+
 ##
 #loop through the text converted pixel output and pick out starting pixels for the each image
 ##
+
 
 i=0
 while read line; do
@@ -38,14 +41,19 @@ for e in "${!START_POINT[@]}"
 do
     index="$e"
     pixel_val="${START_POINT[$e]}"
-    red_single="red_$index_$IMAGE_FILE"
-    sep_single="card_$index_$IMAGE_FILE"
-    mask="mask_$index_$IMAGE_FILE"
+    red_single="red_$index_$3"
+    sep_single="card_$index_$3"
+    mask="mask_$index_$3"
     res_image="card_$index.png"
 
-    convert "$SHADOW__FILE" -fill red -bordercolor white -draw "color "$pixel_val" filltoborder" "$red_single"
+
+    echo "creating red outline for index: $index, $pixel_val"
+    convert "$SHADOW_FILE" -fill red -bordercolor white -draw "color $pixel_val filltoborder" "$red_single"
+    echo "separating that one out"
     convert "$red_single" -channel R -separate "$sep_single"
+    echo "creating image mask"
     convert "$red_single" "$sep_single" -compose subtract -threshold 50% -composite -morphology Dilate Square -negate "$mask"
+    echo "applying mask"
     convert "$mask" "$IMAGE_FILE" -compose Screen -composite -trim "$res_image"
 done
 
