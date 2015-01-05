55fad7292d2c973cadf4543c0b7417ddd168bfc9
drew gordon <drew.s.gordon@gmail.com>
2015-01-05 18:05:37 -0500

    get keywords from pubmed with a doi

---
+++ tools/scripts/utils/doi2mesh.py
@@ -0,0 +1,135 @@
+import urllib.request
+import urllib.parse
+from xml.dom import minidom
+
+
+
+BASE_URL = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/'
+SEARCH_PATH = 'esearch.fcgi?'
+FETCH_PATH = 'efetch.fcgi?'
+
+
+def fetchData(url):
+
+    res = urllib.request.urlopen(url).read()
+    xml = minidom.parseString(res)
+    return xml
+
+
+def getSearchResults(base, search_path, term, database="pubmed"):
+
+    params = {
+        'db': database,
+        'term': term,
+    }
+
+    url = base + search_path + urllib.parse.urlencode(params)
+
+    xml = fetchData(url)
+
+    print("now running: ", term)
+
+    if xml.getElementsByTagName('PhraseNotFound') != []:
+        recordID = -999
+
+    elif xml.getElementsByTagName('Id') == []:
+        recordID = -9999
+
+    else:
+        recordID = xml.getElementsByTagName('Id')[0].childNodes[0].data
+
+    return recordID
+
+
+def getFullRecord(base, fetch_path, recordID, retformat="xml", database="pubmed"):
+
+    if recordID == -999:
+        return 0
+
+    elif recordID == -9999:
+        return 0
+
+    else:
+
+        params = {
+            'db': database,
+            'id': recordID,
+            'retmode': retformat
+        }
+
+        url = base + fetch_path + urllib.parse.urlencode(params)
+
+        xml = fetchData(url)
+
+        
+        meshHeadings = xml.getElementsByTagName('MeshHeading')
+        keywords = xml.getElementsByTagName('Keyword')
+        
+
+        if meshHeadings is not []:
+            mesh_results = []
+            for i in range(len(meshHeadings)):
+                desc = meshHeadings[i].getElementsByTagName('DescriptorName')
+                qual = meshHeadings[i].getElementsByTagName('QualifierName')
+
+
+                desc = desc[0].childNodes[0].data if desc != [] else None
+                qual = qual[0].childNodes[0].data if qual != [] else None
+
+                if qual is not None:
+                    mesh_results.append(desc+'--'+qual)
+                else:
+                    mesh_results.append(desc)
+                               
+        else:
+            mesh_results = 0
+
+        if keywords is not []:
+            keyword_results = []
+            for k in range(len(keywords)):
+                keyw = keywords[0].childNodes[0].data
+                keyword_results.append(keyw)
+        else:
+            keyword_results = 0
+
+        return (mesh_results, keyword_results)
+
+
+
+
+if __name__ == '__main__':
+    import csv
+    import sys, os
+
+    f = sys.argv[1]
+
+    with open(f, 'rt') as c:
+        reader = csv.reader(c)
+        writer = csv.writer(open('./vols_with_keywords.csv', 'wt'))
+        header = next(reader)
+
+        header.extend(['mesh', 'keywords'])
+        writer.writerow(header)
+
+        for row in reader:
+            if row[2] != '':
+                doi = row[2].strip().replace('doi:', '')
+                results = getFullRecord(BASE_URL, FETCH_PATH, getSearchResults(BASE_URL, SEARCH_PATH, doi))
+
+                if results != 0:
+                    mesh = ",".join(results[0])
+                    keyw = ",".join(results[1])
+                    row.extend([str(mesh), str(keyw)])
+
+                    writer.writerow(row)
+                else:
+                    mesh = ""
+                    keyw = ""
+                    row.extend([str(mesh), str(keyw)])
+                    writer.writerow(row)
+
+
+
+
+
+
