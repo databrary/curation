93d843d72ba3876118de9c9761fc4bd1343d6778
drew gordon <drew.s.gordon@gmail.com>
2015-04-01 15:40:27 -0400

    formatting and some processing of creators

--- tools/scripts/utils/ezid/databraryDOI.py
+++ tools/scripts/utils/ezid/databraryDOI.py
@@ -3,6 +3,8 @@ import psycopg2
 import hashlib
 from config import conn as c
 
+target_path = "https://example.org"
+
 
 '''Querying and creating DOIs for Databrary resources. 
    Relevant fields for sending to datacite:
@@ -11,6 +13,19 @@ from config import conn as c
    		datacite.creator: <party name(s)> (who is a "creator")
    		datacite.publicationyear (when shared?)
    		datacite.type: Dataset'''
+
+sqlQuery = ("SELECT v.id as target, v.name as title, COALESCE(prename || ' ', '') || sortname as creator, p.id as party_id, va1.individual as access "
+            "FROM volume_access va1 "
+            "JOIN ("
+            "SELECT DISTINCT volume "
+            "FROM volume_access va "
+            "WHERE (va.party = 0 AND va.individual = 'SHARED') OR (va.party = -1 AND va.individual = 'PUBLIC') "
+            ") va2 USING (volume) "
+            "INNER JOIN volume v ON v.id = va1.volume "
+            "INNER JOIN party p ON p.id = va1.party "
+            "WHERE va1.individual = 'ADMIN' "
+            "ORDER BY target;"
+            ) 
 
 def _makeHash(record:dict) -> str:
 	return hashlib.sha256(str(record).encode('UTF-8')).hexdigest()
@@ -26,50 +41,47 @@ def makeConnection():
 
 	return conn.cursor()
 
-def queryAll(cursor):
-	try:
-		#TODO: make sure this correct
-		#TODO: need to process this afterwards
-	    cursor.execute("""SELECT  v.id as target, v.name as title, p.name as creator, p.id as party_id, va1.individual as access 
-												FROM volume_access va1
-												JOIN (
-    											SELECT DISTINCT volume
-    											FROM volume_access
-    											WHERE (party = 0 AND individual = 'SHARED')
-    											OR (party = -1 AND individual = 'PUBLIC')
-												) va2 USING (volume)
-												INNER JOIN volume v ON v.id = va1.volume
-												INNER JOIN party p ON p.id = va1.party
-												WHERE va1.individual = 'ADMIN'
-												ORDER BY target;""")
-	except:
-		print("Query failed")
+def queryAll(cursor) -> list:
+    try:
+        cursor.execute(sqlQuery)
+    except:
+        print("Query failed")
+    rows = cursor.fetchall()
+    return rows
 
-	rows = cursor.fetchall()
-	return rows
-
-def makeMetadata(rows):
-	mdPayload = []
-	target_base = "https://example.org/volume/"
-	for r in rows:
-		'''check parity here'''
-		mdPayload.append({"_target": target_base + str(r[0]), 
-						  "_profile": "datacite", 
-						  "_status": "reserved", 
-						  "datacite.publisher":"exampleDB", 
-						  "datacite.title":r[1][0:4], 
-						  "datacite.creator":"Anonymous"
-			})
-	return mdPayload
+def getCreators(rs:list) -> dict:
+    '''compile all admin for a volume into a list of creators per volume'''
+    creators = {r[0]:[] for r in rs}
+    for r in rs:
+        creators[r[0]].append(r[2])
+    return creators
 
 
-def postData(payload):
+def makeMetadata(rs:list) -> list:
+    mdPayload = []
+    target_base = target_path + "/volume/"
+    creators = getCreators(rs)
+    for r in rs:
+        '''check parity here'''
+        mdPayload.append({"_target": target_base + str(r[0]), 
+                          "_profile": "datacite", 
+                          "_status": "reserved", 
+                          "datacite.publisher":"Databrary", 
+                          "datacite.title":r[1], 
+                          "datacite.creator":('; ').join(creators[r[0]])
+                        })
+    return mdPayload
+
+
+def postData(payload:list):
 	ezid_doi_session = ezid_api.ApiSession(scheme='doi')
 	for p in payload:
-		ezid_doi_session.mint(p)
+		print("I AM NOW SENDING THIS TO THE EZID SERVER [%s]: %s" % (ezid_doi_session, p))
+    #ezid_doi_session.mint(p)
 
 if __name__ == "__main__":
-	cur = makeConnection()
-	rows = queryAll(cur)
+	print('What do you even want me to do with that?')
+    #cur = makeConnection()
+	#rows = queryAll(cur)
 
 
