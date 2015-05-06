d88807f9135bf2989c7c4149c845b042fe9eddc3
drew gordon <drew.s.gordon@gmail.com>
2015-05-06 11:18:40 -0400

    change where logs will go

--- tools/scripts/utils/video_integrity.py
+++ tools/scripts/utils/video_integrity.py
@@ -7,7 +7,8 @@ import time
 FILE_DIR = sys.argv[1] if not sys.argv[1].endswith('/') else sys.argv[1][:-1]
 FFMPEG_BIN = 'ffmpeg'
 LOG_FILE = FILE_DIR.split('/')[-1] + '_errors_' + str(int(time.time())) + '.log'
-LOG_PATH = os.path.join('../../../data/logs', LOG_FILE)
+LOG_DIR = os.path.expanduser('~/curation_logs/')
+LOG_PATH = LOG_DIR + LOG_FILE
 
 def getFiles(path:str) -> list:
     filesList=[]
