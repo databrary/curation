df9d9c47e9d6c5214b3a4092144454ebbeb030f2
drew gordon <drew.s.gordon@gmail.com>
2015-06-30 18:48:27 -0400

    add duration to file_times output

--- tools/scripts/utils/file_times.sh
+++ tools/scripts/utils/file_times.sh
@@ -5,15 +5,16 @@
 ##
 
 target="$1"
-echo "FILE,FFPROBE_CREATED,STATTIME"
+echo "FILE,FFPROBE_CREATED,STATTIME,DURATION"
 for file in "$target"/*
 do
     base=`basename "$file"`
     if [[ -f $file && $base = *.* ]]; then
       FFPROBE=$(ffprobe -loglevel warning -show_format -of flat -i "$file" | grep '^format\.tags\.creation_time=')
+      DURATION=$(ffprobe -loglevel warning -show_format -of flat -i "$file" | grep '^format\.duration=')
       STAT=$(stat -c "%y" "$file")
       
-      echo "\"$base\",${FFPROBE#*=},\"${STAT%.*}\""
+      echo "\"$base\",${FFPROBE#*=},\"${STAT%.*}\",${DURATION#*=}"
       
     fi
   
