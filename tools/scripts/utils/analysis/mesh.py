464e59fa6d9e44b0f05d71615129264f9d13637d
drew gordon <drew.s.gordon@gmail.com>
2015-09-24 01:33:46 -0400

    this actually returns more detailed results...
    but i don't know why this is returning vastly different (and more limited) results than http://id.nlm.nih.gov/mesh/query?query=PREFIX+rdf%3A+%3Chttp%3A%2F%2Fwww.w3.org%2F1999%2F02%2F22-rdf-syntax-ns%23%3E%0D%0APREFIX+rdfs%3A+%3Chttp%3A%2F%2Fwww.w3.org%2F2000%2F01%2Frdf-schema%23%3E%0D%0APREFIX+xsd%3A+%3Chttp%3A%2F%2Fwww.w3.org%2F2001%2FXMLSchema%23%3E%0D%0APREFIX+owl%3A+%3Chttp%3A%2F%2Fwww.w3.org%2F2002%2F07%2Fowl%23%3E%0D%0APREFIX+meshv%3A+%3Chttp%3A%2F%2Fid.nlm.nih.gov%2Fmesh%2Fvocab%23%3E%0D%0APREFIX+mesh%3A+%3Chttp%3A%2F%2Fid.nlm.nih.gov%2Fmesh%2F%3E%0D%0APREFIX+mesh2015%3A+%3Chttp%3A%2F%2Fid.nlm.nih.gov%2Fmesh%2F2015%2F%3E%0D%0A%0D%0ASELECT+%3Fd+%3FdName+%3Fc+%3Fn%0D%0AFROM+%3Chttp%3A%2F%2Fid.nlm.nih.gov%2Fmesh%3E%0D%0AWHERE+{%0D%0A++%3Fc+meshv%3AscopeNote+%3Fn+.%0D%0A++%3Fd+meshv%3Aconcept+%3Fc+.%0D%0A++%3Fd+rdfs%3Alabel+%3FdName+.%0D%0A++FILTER%28REGEX%28%3FdName%2C%27Infant%27%2C%27i%27%29%29%0D%0A}+%0D%0A&format=HTML&inference=true&year=current&limit=1000&offset=0#lodestart-sparql-results

--- tools/scripts/utils/analysis/mesh.py
+++ tools/scripts/utils/analysis/mesh.py
@@ -10,20 +10,22 @@ def main():
 
 def getMesh(term):
     qString = """ 
-                  PREFIX mesh: <http://id.nlm.nih.gov/mesh/>
-                  PREFIX meshv: <http://id.nlm.nih.gov/mesh/vocab#>
+                  PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
                   PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+                  PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+                  PREFIX owl: <http://www.w3.org/2002/07/owl#>
+                  PREFIX meshv: <http://id.nlm.nih.gov/mesh/vocab#>
+                  PREFIX mesh: <http://id.nlm.nih.gov/mesh/>
+                  PREFIX mesh2015: <http://id.nlm.nih.gov/mesh/2015/>
 
-                  SELECT *
+                  SELECT ?d ?dName ?c ?n
                   FROM <http://id.nlm.nih.gov/mesh>
-                  
                   WHERE {
-                
-                  ?d rdfs:label ?dName .
-                  FILTER(REGEX(?dName,"%s","i"))
-                  
-                  }
-                  ORDER BY ?d """ % (term)
+                    ?c meshv:scopeNote ?n .
+                    ?d meshv:concept ?c .
+                    ?d rdfs:label ?dName .
+                    FILTER(REGEX(?dName,'%s','i'))
+                   } """ % (term)
                   
     sparql = SPARQLWrapper("http://id.nlm.nih.gov/mesh/sparql")
     sparql.setQuery(qString)
