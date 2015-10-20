b79eed5225003421c76ffa4962eaf5036d145349
drew gordon <drew.s.gordon@gmail.com>
2015-10-20 18:50:36 -0400

    this should capture all the terms we'll need
    but may even want to limit to specific qualifiers

--- tools/scripts/utils/analysis/mesh_scraper.py
+++ tools/scripts/utils/analysis/mesh_scraper.py
@@ -1,6 +1,9 @@
 import requests
 from lxml import html
+import csv, json
+import time
 
+BASE = 'https://www.nlm.nih.gov'
 HOME = 'https://www.nlm.nih.gov/mesh/trees.html'
 
 '''only the categories we want, with their order on the home page'''
@@ -19,22 +22,68 @@ category_map = {
 }
 
 
-def get_home(url):
+def get_page_tree(url):
     page = requests.get(url)
     return html.fromstring(page.text)
 
+def save_all(data):
+    with open("./data/mesh_terms.csv", 'wt') as f:
+        w = csv.writer(f)
+        for d in data:
+            w.writerow(d)
+
+def getdID(treeNum):
+    _base = "http://id.nlm.nih.gov/mesh/sparql"
+    _q = (
+        
+        "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> "
+        "PREFIX meshv: <http://id.nlm.nih.gov/mesh/vocab#> "
+        "SELECT ?d "
+        "FROM <http://id.nlm.nih.gov/mesh> "
+        "WHERE {{ "
+        "?d a meshv:Descriptor ."
+        "?d meshv:treeNumber ?q ."
+        "?q rdfs:label '{0}'@en"
+        "}}"   
+    ).format(treeNum)
+    _inference = "true"
+    _format = "JSON"     
+    params = {"query":_q,
+              "format": _format,
+              "inference": _inference}
+    res = requests.get(url=_base, params=params)
+    if res.status_code == 200:
+        data = json.loads(res.text)
+        dId = data['results']['bindings'][0]['d']['value']
+    else:
+        print "whoops: %s" % res.status
+        dId = "could not get"
+    return dId
+
+
 def get_category_links(d):
-    tree = get_home(HOME)
+    rows = []
+    rows.append(["category", "topic", "tree_header", "term", "tree_number"])
+    tree = get_page_tree(HOME)
     for k,v in d.items():
         _xpath_base = '//div[@id="body"]/ol/li[%s]/' % (v[1])
         _xpath_li = _xpath_base + 'ul/li/text()'
         topics = [x for x in tree.xpath(_xpath_li) if x != ' ']  
         for i in topics:
             topic, tree_head = i.split(' [')
+            tree_head = tree_head.strip()[:-1]
             _xpath_a = _xpath_base + 'ul/li[%s]/a[1]/@href' % (str(int(topics.index(i))+1))
-            link = tree.xpath(_xpath_a)[0]
-            #link = link[0].attrib('href') if link != [] else 'NA?'
-            print k, '=', topic, tree_head[0:3], link
+            link = BASE + tree.xpath(_xpath_a)[0]
+            term_html = get_page_tree(link)
+            terms = term_html.xpath('//div[@id="body"]//li/a/text()')
+            for t in terms:
+                term, treeNum = t.split(' [')
+                treeNum = treeNum[:-1]
+                dID = getdID(treeNum)
+                r = [k, topic, tree_head, term, treeNum, dID]
+                rows.append(r)
+                time.sleep(1) #this is probably excessive, the limit is 500 requests a minute
+    save_all(rows)            
 
 if __name__ == '__main__':
     get_category_links(category_map)
