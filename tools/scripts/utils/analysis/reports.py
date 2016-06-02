4defd794be063a0cc40dd62edb4a3ac2cc815989
drew gordon <drew.s.gordon@gmail.com>
2016-06-02 17:28:18 -0400

    reports script WIP

---
+++ tools/scripts/utils/analysis/reports.py
@@ -0,0 +1,27 @@
+import psycopg2
+from .. import dbclient
+import pandas as pd
+import sys, os, time
+from ..config import conn as c
+from ..config import report_queries as qs
+import re
+import csv
+
+
+class Report(object):
+
+    """
+    Executing a query returns a report 
+    """
+    pass
+
+
+
+
+def main():
+    
+
+
+
+if __name__ == '__main__':
+    main()
